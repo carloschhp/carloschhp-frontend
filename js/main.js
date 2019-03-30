2d74c74b54b9ade913c4e6c038ed5397dc991cbc
Carlos Henrique <carloshenriquechhp@gmail.com>
2019-03-30 18:19:20 -0300

    Adicionado Gulp configurado e arquivos inicias.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+
+	var contact_me    = $(".contact_me");
+	var content_block = $(".content");
+
+	$(contact_me).hover(function(){
+		$(content_block).toggleClass("content--bg");
+	});
+});
